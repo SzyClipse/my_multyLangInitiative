5a27c4ade54df0649c24f5a466cd8ae9056cac77
SzyClipse <elliot.johns@runbox.com>
2025-08-13 17:25:01 +0200

    feat: prepare TypeScript

---
+++ leetcode/leet-msnotjava/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hello = "Hello";
+const world = "World";
+var hello_world = hello + " " + world + "!";
+console.log(' -> ' + hello_world);
+//# sourceMappingURL=index.js.map
