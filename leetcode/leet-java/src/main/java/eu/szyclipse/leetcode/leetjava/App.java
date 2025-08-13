dcd5d6f0b110c7b04609149bd90034de0817c6e7
SzyClipse <elliot.johns@runbox.com>
2025-08-13 17:10:27 +0200

    feat: prepare Java

---
+++ leetcode/leet-java/src/main/java/eu/szyclipse/leetcode/leetjava/App.java
@@ -0,0 +1,10 @@
+package eu.szyclipse.leetcode.leetjava;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
